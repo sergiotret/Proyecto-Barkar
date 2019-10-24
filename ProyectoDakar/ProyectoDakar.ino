cb3c4079f7d44b348c88af4fbaefb7b7c71aa9a8
sergiotret <sergitosoberanis@gmail.com>
2019-10-24 18:51:15 -0500

    Con este codigo ya maneja tanto el de voz, como los sensores

--- ProyectoDakar/ProyectoDakar.ino
+++ ProyectoDakar/ProyectoDakar.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-// Declaramos la variable para controlar el servo
+int foco=13; //usaremos el led de arduino
 Servo servoMotor;
 const int sensorMin = 0;
 const int sensorMax = 1024;
@@ -7,77 +7,69 @@ const int sensormin2 = 0;
 const int sensormax2 = 1024;
 int sensorpin = A1;
 int ledpin = 5;
-int ledpin2 = 3;
+int ledpin2 = 3; 
 int sensorValue = 0;
-const int ledPIN = 3;
+int dato=0;
 void setup() {
-  Serial.begin(9600);    //iniciar puerto serie
-  pinMode(ledPIN , OUTPUT);  //definir pin como salida
-  
+  Serial.begin(9600);
+  pinMode(foco,OUTPUT); //->salida del foco
+  //----------------------------------------------------------------
   Serial.begin(9600);   //iniciar puerto serie
   pinMode(9, INPUT);  //definir pin como entrada
-  
+  //SERVOMOTOR------------------------------------------------------
   servoMotor.attach(10);
   Serial.begin(9600);
-  //para el fotoresistor
+  //leds del FOTORESISTOR--------------------------------------------
   pinMode(ledpin, OUTPUT);
   pinMode(ledpin2, OUTPUT);
   Serial.begin(9600);
 }
-
-void loop(){
-  int sensorReading= analogRead(A0);
-  int range=map(sensorReading, sensorMin, sensorMax,0,3);
-  //Serial.println(sensorReading);
-  switch(range){
- case 0:
-    Serial.println("lloviendo");
-    servoMotor.write(180);
+void loop() {
+  // put your main code here, to run repeatedly:
+  sensorValue = 0;
+  if(Serial.available()>0){
+  dato=Serial.read(); //lo que se lea del bluetooth, es decir lo mandará la app
+  }
+  if(dato == '1')
+  {digitalWrite(foco,HIGH);
+  Serial.println("Foco encendido");
+    }
+  if(dato == '2')
+    {
+      digitalWrite(foco,LOW);
+      Serial.println("foco apagado");
+     }
+if(dato == '3')
+    {
+      digitalWrite(foco,LOW);
+      Serial.println("foco ENCENDIDO");
+      servoMotor.write(90);
+     }
+    int sensorReading= analogRead(A0);
+    //el sensor al no detectar agua regresa a su posicion original
+    if(sensorReading>=1014)
+    {
+      servoMotor.write(90);
+    }
+    //si detecta agyua 
+    if (sensorReading<=670)
+    {
+      servoMotor.write(180);
+    }
     delay(5000);
-    break;
- case 1:
-    Serial.println("alerta de lluvia");
-      //Desplazamos a la posición 180º
-  servoMotor.write(180);
-  delay(5000);
-    break;
- case 2:
-    Serial.println("sin lluvia");
-    delay(2000);
-    break;
-  }
-  delay(1000);
-  Serial.println(sensorReading);
-  if(sensorReading>=1014)
-  {
-    servoMotor.write(90);
-  }
-
-  
-//PARA EL FOTORESISTOR, USE LA MISMA SINTAXIS QUE CON EL SENSOR DE LLUVIA
-//CODIGO SENCILLO, DEPENDIENDO LA CANTIDAD DE LUZ DETERMINA SI ENCIENDE UN LED, O ENCIENDE AMBOS LEDS
-sensorValue = analogRead(sensorpin);
-  int range2=map(sensorValue, sensormin2, sensormax2,0,3);
-  switch(range2){
- case 0:
-    Serial.println("Encender Leds");
-    digitalWrite(ledpin,HIGH);
-    digitalWrite(ledpin2,HIGH);
-    delay(5000);
-    break;
- case 1:
-    Serial.println("encender led");
-      //Desplazamos a la posición 180º
-  digitalWrite(ledpin,LOW);
-  digitalWrite(ledpin2,HIGH);
-  delay(5000);
-    break;
- case 2:
-    Serial.println("Apagar leds");
-    digitalWrite(ledpin,LOW);
-    digitalWrite(ledpin2,LOW);
-    delay(2000);
-    break;
-  }
-  Serial.println(sensorValue, DEC);
+      //PARA EL FOTORESISTOR
+      //CODIGO SENCILLO, DEPENDIENDO LA CANTIDAD DE LUZ DETERMINA SI ENCIENDE UN LED, O ENCIENDE AMBOS LEDS
+      sensorValue = analogRead(sensorpin);
+     // si el sensor no detecta luz, automaticamente se encienden
+     if(sensorValue>=1000)
+    {
+      digitalWrite(ledpin,HIGH);
+      digitalWrite(ledpin2,HIGH);
+    }
+    //si detecta luz, se enciende
+    if (sensorValue<=600)
+    {
+      digitalWrite(ledpin,LOW);
+      digitalWrite(ledpin2,LOW);
+    }
 }
